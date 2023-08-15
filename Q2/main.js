6e63419163e407cca3f6bdea9cba4dc2726f6cfb
oghene-ella <oghenekaro57@gmail.com>
2023-08-15 20:52:06 +0100

    made some changes

--- Q2/main.js
+++ Q2/main.js
@@ -6,6 +6,7 @@ const welcomeJs = require("./modules/welcome.js");
 const getJs = require("./modules/get.js");
 const postJs = require("./modules/post.js");
 const updateJs = require("./modules/update.js");
+const delJs = require("./modules/delete.js");
 const bodyParser = require("./parser.js")
 
 /* Step 2, define the port and host name*/
@@ -24,17 +25,25 @@ server.on("request", (req, res) => {
   }
 
   // the get Method
-  if (req.url === "/inventory" && req.method === "GET") {
+  else if (req.url === "/inventory" && req.method === "GET") {
     getJs.getMethod(req, res);
   }
 
   // the post Method
-  if (req.url === "/inventory" && req.method === "POST") {
+  else if (req.url === "/inventory" && req.method === "POST") {
     postJs.postMethod(req, res);
   }
 
-  if (req.url === "/inventory" && req.method === "PUT") {
+  else if (req.url === "/inventory" && req.method === "PUT") {
     updateJs.updateMethod(req, res);
+  }
+
+  else if (req.url === "/inventory" && req.method === "DELETE") {
+    delJs.delMethod(req, res);
+  }
+
+  else{
+    res.end("Invalid route")
   }
 });
 
