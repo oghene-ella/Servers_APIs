a59bf9dac40fce88df78ff82eaa89be557edaeba
oghene-ella <oghenekaro57@gmail.com>
2023-08-15 19:45:33 +0100

    made some changes

--- Q2/main.js
+++ Q2/main.js
@@ -2,8 +2,10 @@
 const http = require("http");
 
 /* Import the custom modules*/
+const welcomeJs = require("./modules/welcome.js");
 const getJs = require("./modules/get.js");
 const postJs = require("./modules/post.js");
+const updateJs = require("./modules/update.js");
 const bodyParser = require("./parser.js")
 
 /* Step 2, define the port and host name*/
@@ -11,14 +13,14 @@ const hostname = "localhost";
 const port = 4300;
 
 /* Create the server */
-// const server = http.createServer()
 const server = http.createServer((req, res) => bodyParser.parser(req, res))
 
 /* Create a function that handles all the request */
 server.on("request", (req, res) => {
-  // the get Method
+
+  // the index get Method
   if (req.url === "/" && req.method === "GET") {
-    getJs.getMethod(req, res);
+    welcomeJs.welcomeMethod(req, res);
   }
 
   // the get Method
@@ -29,6 +31,10 @@ server.on("request", (req, res) => {
   // the post Method
   if (req.url === "/inventory" && req.method === "POST") {
     postJs.postMethod(req, res);
+  }
+
+  if (req.url === "/inventory" && req.method === "PUT") {
+    updateJs.updateMethod(req, res);
   }
 });
 
